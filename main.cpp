32f1badf4b64cf63f726190dfd411bf75422e3c7
IvanNugiban <87913973+IvanNugiban@users.noreply.github.com>
2024-02-10 13:12:01 +0200

    Init commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QPushButton>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    MainWindow mainWindow{};
+    mainWindow.show();
+
+    return a.exec();
+}
